ff34008c8cd527fd9421a72af6ae9aad38cd9a8f
Andrei Ciortea <andrei.ciortea@gmail.com>
2014-08-22 23:30:43 +0300

    Added Twitter support.

--- app/democlient/DemoClientApplication.scala
+++ app/democlient/DemoClientApplication.scala
@@ -9,9 +9,64 @@ import play.api.data._
 import play.api.data.Forms._
 
 import play.api.Play.current
+import play.api.libs.json._
 import play.api.libs.ws._
 
+import org.scribe.builder._
+import org.scribe.builder.api._
+import org.scribe.model._
+
+
 object DemoClientApplication extends Controller {
+  
+  def runTwitterRequest(method: String, uri: String, jsonData: String): (Int, String) = {
+    val CONSUMER_KEY = "CONSUMER_KEY"
+    val CONSUMER_SECRET = "CONSUMER_SECRET"
+    val TOKEN = "TOKEN"
+    val TOKEN_SECRET = "TOKEN_SECRET"
+
+    val service = new ServiceBuilder()
+                        .provider(classOf[TwitterApi])
+                        .apiKey(CONSUMER_KEY)
+                        .apiSecret(CONSUMER_SECRET)
+                        .build()
+      
+    val jObjData = Json.parse(jsonData).as[JsObject]
+    
+    def cookPostRequest(request: OAuthRequest) = {
+      for (k <- jObjData.keys) {
+        request.addBodyParameter(k, (jObjData \ k).as[String])
+      }
+      
+      request
+    }
+    
+    val request =
+        method match {
+      
+          case "GET" => ((request: OAuthRequest) => {
+            for (k <- jObjData.keys) {
+              request.addQuerystringParameter(k, (jObjData \ k).as[String])
+            }
+            
+            request
+          })(new OAuthRequest(Verb.GET, uri))
+          
+          case "POST" => ((request: OAuthRequest) => {
+            for (k <- jObjData.keys) {
+              request.addBodyParameter(k, (jObjData \ k).as[String])
+            }
+            
+            request
+          })(new OAuthRequest(Verb.POST, uri))
+        }
+    
+    service.signRequest(new Token(TOKEN, TOKEN_SECRET), request)
+    val response = request.send
+
+    (response.getCode, response.getBody)
+  }
+
   
   val stnForm = Form(
     single(
@@ -65,15 +120,23 @@ object DemoClientApplication extends Controller {
         val requestUri = result._4
         val jsonData = result._5
         
-        runHttpCall(webid, method, requestUri, jsonData).map {
-          response =>
-            Ok("Status code:" + response.status + "\n" + response.body)
+        if (requestUri.startsWith("https://api.twitter.com")) {
+          (
+              (
+                  response: (Int, String)) =>
+                    future{ Ok("Status code:" + response._1 + "\n" + response._2) }
+          )(runTwitterRequest(method, requestUri, jsonData))
+        } else {
+          runHttpRequest(webid, method, requestUri, jsonData).map {
+            response =>
+              Ok("Status code:" + response.status + "\n" + response.body)
+          }
         }
       }
     )
   }
   
-  def runHttpCall(webid: String, method: String, uri: String, jsonData: String) = {
+  def runHttpRequest(webid: String, method: String, uri: String, jsonData: String) = {
     val holder: WSRequestHolder = WS.url(uri)
     
     val futureResponse = 
